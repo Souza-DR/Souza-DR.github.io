2209f20d2eb2442fdf5cc79cd6fccaa2dca9f5fa
Souza-DR <78029741+Souza-DR@users.noreply.github.com>
2026-05-04 18:06:24 +0000

    Deploying to gh-pages from @ Souza-DR/Souza-DR.github.io@9603586ef4f979090002dd5d28e35f3964c994af 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -556,13 +556,6 @@ ninja.data = [{
           window.open("https://www.scopus.com/authid/detail.uri?authorId=59412730300", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
